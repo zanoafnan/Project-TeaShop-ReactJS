d84aad3dc914939226642d71151d62f46032f7a2
Muhammad Zano Afnan H <zanoafnan@students.unnes.ac.id>
2023-12-12 15:18:13 +0700

    Fixing background image source

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
         backgroundColor: "#8b997f",
       },
       backgroundImage: {
-        'home': "url('src/assets/img/hometea.jpg')",
-        'about': "url('src/assets/img/aboutbg.jpg')",
+        'home': "url('/src/assets/img/hometea.jpg')",
+        'about': "url('/src/assets/img/aboutbg.jpg')",
       }
     },
   },
